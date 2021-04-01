b5e1e843b59958e8f3a4a40ce7c21da496c29c59
soto <miloosoto@gmail.com>
2021-04-01 13:15:10 -0500

    release version 1.7

---
+++ app/src/main/java/com/grade/quickid/model/comentarios/infraestructure/ComentarioActivity.java
@@ -0,0 +1,86 @@
+
+package com.grade.quickid.model.comentarios.infraestructure;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.text.InputType;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.grade.quickid.R;
+import com.grade.quickid.model.MainActivity;
+import com.grade.quickid.model.Time;
+import com.grade.quickid.model.comentarios.domain.Comentario;
+import com.grade.quickid.model.personas.infrastructure.LoginActivity;
+
+import java.util.UUID;
+
+public class ComentarioActivity extends AppCompatActivity {
+private Button enviarComentario;
+private TextView textoComentario;
+DatabaseReference myRef;
+FirebaseDatabase firebaseDatabase;
+DatabaseReference databaseReference;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_comentario);
+        textoComentario = findViewById(R.id.textviewComentario);
+        textoComentario.setHint(" Comparte tu comentario");
+        enviarComentario = findViewById(R.id.btn_enviarComentario);
+        inicializarFirebase();
+        enviarComentario.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               String coment =  textoComentario.getText().toString();
+                Comentario comentario = new Comentario();
+                String clave= UUID.randomUUID().toString();
+                if(coment != null && coment.length()>0){
+                    final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                    Time time = new Time();
+                    comentario.setComentario(coment);
+                    myRef = databaseReference.child("Comentario");
+                    comentario.setFechaComentario(time.fecha());
+                    comentario.setIdCOmentario(clave);
+                    comentario.setIdUsuario(user.getUid()+" "+user.getDisplayName());
+                    myRef.child(clave).setValue(comentario);
+                    Toast.makeText(ComentarioActivity.this,"Gracias por tu comentario",Toast.LENGTH_LONG).show();
+                    textoComentario.setText("");
+                }else{
+                    Toast.makeText(ComentarioActivity.this,"comentario vacio",Toast.LENGTH_LONG).show();
+                }
+
+
+            }
+        });
+
+    }
+
+    private void inicializarFirebase() {
+        FirebaseApp.initializeApp(this);
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        // firebaseDatabase.setPersistenceEnabled(true);
+        databaseReference= firebaseDatabase.getReference();
+    }
+
+    @Override
+    public void onBackPressed() {
+        Intent intent = new Intent(ComentarioActivity.this, MainActivity.class);
+        startActivity(intent);
+        this.finish();
+    }
+}
